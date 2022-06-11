38b5beff7cfd8bcd05f3b82f59bc72882e0731fc
ConradoClarke <conradoclarke@gmail.com>
2022-06-11 18:18:02 -0300

    DashCrystal Door

---
+++ Assets/Scripts/Scenery/DashCrystalDoor.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Licht.Impl.Orchestration;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class DashCrystalDoor : RoomObject
+{
+    public ScriptableTriggerWatcher TriggerWatcher;
+    public Animator Animator;
+    public Animator HandleAnimator;
+    public RoomExit TargetRoomExit;
+    private Player _player;
+    public Collider2D[] Colliders;
+
+    public override void PerformDestroy()
+    {
+        TriggerWatcher.OnTriggerChanged -= TriggerWatcher_OnTriggerChanged;
+    }
+
+    public override bool PerformReset()
+    {
+        return true;
+    }
+
+    public override void Initialize()
+    {
+        _player = Player.Instance();
+        TriggerWatcher.OnTriggerChanged += TriggerWatcher_OnTriggerChanged;
+    }
+
+    private void TriggerWatcher_OnTriggerChanged(bool obj)
+    {
+        HandleAnimator.SetBool("Open", obj);
+        ChangeColliderState(!obj);
+    }
+
+    public override bool Activate()
+    {
+        HandleAnimator.SetBool("Open", TriggerWatcher.Trigger.Triggered);
+        ChangeColliderState(!TriggerWatcher.Trigger.Triggered);
+        return true;
+    }
+
+    private void ChangeColliderState(bool state)
+    {
+        foreach (var col in Colliders)
+        {
+            col.enabled = state;
+        }
+    }
+}
